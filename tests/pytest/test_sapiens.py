196163e07e0234e58e81903b6dc6b4f52fe0fb3a
David Prihoda <david.prihoda1@merck.com>
2022-03-29 11:29:54 +0200

    Add humanization test

--- tests/pytest/test_sapiens.py
+++ tests/pytest/test_sapiens.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sapiens
+from abnumber import Chain
+from biophi.humanization.methods.humanization import humanize_chain, SapiensHumanizationParams
 
 
 def test_sapiens_predict():
@@ -11,3 +13,17 @@ def test_sapiens_predict():
     )
     assert pred.shape == (len(seq), 20), 'Expected matrix (length of sequence * 20 amino acids)'
     assert (pred.idxmax(axis=1).values == np.array(list(seq))).sum() > 100, 'Prediction should be similar to input sequence'
+
+
+def test_sapiens_humanize():
+    seq = 'QVQLVQSGVEVKKPGASVKVSCKASGYTFTNYYMYWVRQAPGQGLEWMGGINPSNGGTNFNEKFKNRVTLTTDSSTTTAYMELKSLQFDDTAVYYCARRDYRFDMGFDYWGQGTTVTVSS'
+    chain = Chain(seq, scheme='kabat')
+    humanization_params = SapiensHumanizationParams(
+        model_version='latest',
+        humanize_cdrs=False,
+        scheme='kabat',
+        cdr_definition='kabat',
+        iterations=1
+    )
+    humanization = humanize_chain(chain, params=humanization_params)
+    assert humanization.humanized_chain.seq == 'QVQLVQSGAEVKKPGASVKVSCKASGYTFTNYYMYWVRQAPGQGLEWMGGINPSNGGTNFNEKFKNRVTLTTDTSTTTAYMELRSLRSDDTAVYYCARRDYRFDMGFDYWGQGTLVTVSS'
